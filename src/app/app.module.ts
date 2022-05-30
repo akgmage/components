4f77c1053c318a155ef62e4f28c74f0372fad07c
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-30 23:27:25 +0530

    Import CollectionsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CollectionsModule } from './collections/collections.module';
 import { ElementsModule } from './elements/elements.module';
 
 @NgModule({
@@ -12,8 +13,9 @@ import { ElementsModule } from './elements/elements.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ElementsModule // Reach out to ElementsModule, get access to all the components, that
+    ElementsModule, // Reach out to ElementsModule, get access to all the components, that
     // have been exported from it and give us access to them inside of the app module
+    CollectionsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
