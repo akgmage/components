e37cfb8ac2b01876eb173481fe776daf1d2a7503
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-31 09:31:47 +0530

    Import EleemntsHomeComponent and add route/path to elements

--- src/app/elements/elements-routing.module.ts
+++ src/app/elements/elements-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
+const routes: Routes = [
+  { path: 'elements', component: ElementsHomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
