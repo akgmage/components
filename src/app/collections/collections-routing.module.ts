da0e1e411dcde5f5d900113d0306bcf4b9783e39
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-31 22:48:56 +0530

    Import CollectionsHomeComponent and add route/path to collections

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'collections', component: CollectionsHomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
