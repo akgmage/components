5eea36210ac0938901d8c841b715f02209750733
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-01 23:08:08 +0530

    Add lazy loaded child route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,10 @@ const routes: Routes = [
     path: 'elements', 
     loadChildren: () => import('./elements/elements.module').then(m => m.ElementsModule)
   },
+  {
+    path: 'collections',
+    loadChildren: () => import('./collections/collections.module').then(m => m.CollectionsModule)
+  },
   { path: '', component: HomeComponent},
   { path: '**', component: NotFoundComponent}
 ];
