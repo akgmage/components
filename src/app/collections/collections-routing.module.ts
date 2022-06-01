59d4e9e9f04aa76adbc181aa7f82aac75c795c68
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-01 23:08:21 +0530

    Set the path of route to be relative to the path specified in AppRoutingModule

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 
 const routes: Routes = [
-  { path: 'collections', component: CollectionsHomeComponent}
+  { path: '', component: CollectionsHomeComponent}
 ];
 
 @NgModule({
