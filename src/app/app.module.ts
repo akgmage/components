3a65306826cd30ec6cbad300cfbc00fba7c30134
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-01 23:07:50 +0530

    Remove CollectionsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CollectionsModule } from './collections/collections.module';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
@@ -17,7 +16,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     BrowserModule,
     //ElementsModule, // Reach out to ElementsModule, get access to all the components, that
     // have been exported from it and give us access to them inside of the app module
-    CollectionsModule,
+    //CollectionsModule,
     AppRoutingModule
   ],
   providers: [],
