ea04883687225596de3d504638ef4195fc4d2672
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-02 22:53:41 +0530

    Add Divider component inside Shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DividerComponent } from './divider/divider.component';
+
+
+
+@NgModule({
+  declarations: [
+    DividerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
