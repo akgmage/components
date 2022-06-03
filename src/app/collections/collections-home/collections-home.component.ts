39683692bf70d9af36b93dd02cf68ce95f3aeedb
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-03 23:36:40 +0530

    Add new key value pair to both data and headers

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,12 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
   data = [
-    { name: 'asdasd', age: 23, job: 'Noob' },
-    { name: 'asdasd', age: 23, job: 'Noob' },
-    { name: 'asdasd', age: 23, job: 'Noob' },
+    { name: 'asdasd', age: 25, job: 'Noob1', employed: true },
+    { name: 'asddfg', age: 24, job: 'Noob2', employed: false },
+    { name: 'fghjfgh', age: 23, job: 'Noob3', employed:false },
   ];
 
   headers = [
+    { key: 'employed', label: 'Has a job?' },
     { key: 'name', label: 'Name' },
     { key: 'age', label: 'Age' },
     { key: 'job', label: 'Job' },
