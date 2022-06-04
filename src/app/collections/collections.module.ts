d8278e5dc4c9d0a7fefdbe5bf4f248bd0d8d5bb3
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-04 21:21:19 +0530

    Add Biography component inside collections module

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -5,12 +5,14 @@ import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 import { TableComponent } from './table/table.component';
 import { SharedModule } from '../shared/shared.module';
+import { BiographyComponent } from './biography/biography.component';
 
 
 @NgModule({
   declarations: [
     CollectionsHomeComponent,
-    TableComponent
+    TableComponent,
+    BiographyComponent
   ],
   imports: [
     CommonModule,
