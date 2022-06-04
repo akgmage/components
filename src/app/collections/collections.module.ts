a4d845f19a2f92f8965f63e913abc2afddb3a01c
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-04 21:22:16 +0530

    Add Partners component inside collections module

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -7,6 +7,7 @@ import { TableComponent } from './table/table.component';
 import { SharedModule } from '../shared/shared.module';
 import { BiographyComponent } from './biography/biography.component';
 import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { CompaniesComponent } from './companies/companies.component';
     CollectionsHomeComponent,
     TableComponent,
     BiographyComponent,
-    CompaniesComponent
+    CompaniesComponent,
+    PartnersComponent
   ],
   imports: [
     CommonModule,
