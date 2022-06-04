9dafaaf5dc05aa9f6ba4e381d5a90702fb124a38
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-04 21:33:10 +0530

    Add child routes inside CollectionsHomeComponent

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 const routes: Routes = [
-  { path: '', component: CollectionsHomeComponent}
+  { 
+    path: '', 
+    component: CollectionsHomeComponent,
+    // conditionally show three different components
+    children: [
+      { path: '', component: BiographyComponent}, // show biography by default
+      { path: 'companies', component: CompaniesComponent},
+      { path: 'partners', component: PartnersComponent},
+
+    ]
+  }
 ];
 
 @NgModule({
