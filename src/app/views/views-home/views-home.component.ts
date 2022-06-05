2742a1b590bf2bcbb89c3b1157097febc96ab765
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-05 20:07:12 +0530

    Add items array of objects

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -11,6 +11,19 @@ export class ViewsHomeComponent implements OnInit {
     { value: 2900, label: 'Revenue' },
     { value: 500, label: 'Reviews' },
   ];
+  items = [
+    { 
+      image: '/assets/images/couch.jpeg', 
+      title: 'Couch',
+      description: 'This is a fantastic couch to sit on'
+    },
+    { 
+      image: '/assets/images/dresser.jpeg', 
+      title: 'Couch',
+      description: 'This is a great dresser to put stuff in'
+    },
+
+  ];
   constructor() { }
 
   ngOnInit(): void {
