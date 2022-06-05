da83c40c26f88479180854f75dc98c55371f18ba
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-05 19:56:36 +0530

    Add ItemList component inside views module

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -5,11 +5,13 @@ import { ViewsRoutingModule } from './views-routing.module';
 import { ViewsHomeComponent } from './views-home/views-home.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { SharedModule } from '../shared/shared.module';
+import { ItemListComponent } from './item-list/item-list.component';
 
 @NgModule({
   declarations: [
     ViewsHomeComponent,
-    StatisticsComponent
+    StatisticsComponent,
+    ItemListComponent
   ],
   imports: [
     CommonModule,
