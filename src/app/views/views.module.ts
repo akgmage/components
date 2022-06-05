4bba592e124051c51c124829eafe593c45fdb9de
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-05 14:25:59 +0530

    Import SharedModule

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ViewsRoutingModule } from './views-routing.module';
 import { ViewsHomeComponent } from './views-home/views-home.component';
 import { StatisticsComponent } from './statistics/statistics.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +13,8 @@ import { StatisticsComponent } from './statistics/statistics.component';
   ],
   imports: [
     CommonModule,
-    ViewsRoutingModule
+    ViewsRoutingModule,
+    SharedModule
   ]
 })
 export class ViewsModule { }
