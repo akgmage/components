6cf7d42bff486d86c79cc6a43c7590813e51ac77
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-05 20:09:01 +0530

    Expect item from parent

--- src/app/views/item-list/item-list.component.ts
+++ src/app/views/item-list/item-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item-list',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-list.component.css']
 })
 export class ItemListComponent implements OnInit {
-
+  @Input() items:any[]= [];
   constructor() { }
 
   ngOnInit(): void {
