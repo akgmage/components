eb3c7c1d924336495fa4bdd69b45518c5acfc17d
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-06 22:47:58 +0530

    Add a rule to the ModsRouting Module to show the ModsHomeComponent

--- src/app/mods/mods-routing.module.ts
+++ src/app/mods/mods-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ModsHomeComponent } from './mods-home/mods-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ModsHomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
